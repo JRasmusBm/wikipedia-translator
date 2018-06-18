21f30eac8ea90c6bf0b225019d68e31933f6bc51
jrasmusbm <JRasmusBm@gmail.com>
2018-06-18 09:39:49 +0200

    Add command line arguments and apply pep8

--- wikipedia-translator.py
+++ wikipedia-translator.py
@@ -1,7 +1,9 @@
 from bs4 import BeautifulSoup
+from sys import argv
 from os.path import join
 from urllib.parse import parse_qsl
 import requests
+
 
 def translate(word, target_language="en"):
 
@@ -10,27 +12,32 @@ def translate(word, target_language="en"):
 
     google_response = requests.get(google_url, timeout=5)
     google_content = BeautifulSoup(google_response.content, "html.parser")
+
     wikipedia_link = ""
     for a in google_content.find_all("a", href=True):
         if "wikipedia.org" in a["href"]:
             wikipedia_link = parse_qsl(a["href"])[0][1]
             break
-    if wikipedia_link == "" :
+
+    if wikipedia_link == "":
         raise Exception("No translation found!")
     wikipedia_response = requests.get(wikipedia_link)
-    wikipedia_content = BeautifulSoup(wikipedia_response.content, "html.parser")
+    wikipedia_content = BeautifulSoup(wikipedia_response.content,
+                                      "html.parser")
 
-    lang_node = wikipedia_content.find(id="p-lang").find("a", lang=target_language)
+    lang_node = wikipedia_content.find(id="p-lang").find(
+        "a", lang=target_language)
 
-    if lang_node != None :
+    if lang_node is not None:
         wikipedia_link = lang_node["href"]
         wikipedia_response = requests.get(wikipedia_link)
-        wikipedia_content = BeautifulSoup(wikipedia_response.content, "html.parser")
+        wikipedia_content = BeautifulSoup(wikipedia_response.content,
+                                          "html.parser")
 
-    return(wikipedia_content.find(id="firstHeading").text)
+    return (wikipedia_content.find(id="firstHeading").text)
 
-try:
-    print(translate("Mjukglass", "en"))
-except Exception as e:
-    print(e)
 
+if len(argv) == 3:
+    print(translate(argv[1], argv[2]))
+elif len(argv) == 2:
+    print(translate(argv[1]))
