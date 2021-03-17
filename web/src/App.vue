15cc282477186cf64f15861c2bae05a5275b140e
jrasmusbm <JRasmusBm@gmail.com>
2021-03-17 13:59:12 +0100

    Make UI for translation

--- web/src/App.vue
+++ web/src/App.vue
@@ -1,26 +1,50 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Welcome to Your Vue.js App" />
+  <WikipediaTranslator />
 </template>
 
-<script>
-import HelloWorld from "./components/HelloWorld.vue";
+<script lang="ts">
+import { defineComponent } from "vue";
 
-export default {
+import WikipediaTranslator from "./components/WikipediaTranslator.vue";
+
+export default defineComponent({
   name: "App",
   components: {
-    HelloWorld
-  }
-};
+    WikipediaTranslator,
+  },
+});
 </script>
 
 <style>
+* {
+  padding: 0;
+  margin: 0;
+  box-sizing: border-box;
+}
+
+body {
+  --background: #272914;
+  --color: #f8f4e3;
+  --search-border: #d5573b;
+  --search-background: #648844;
+}
+
+@media (prefers-color-scheme: light) {
+  body {
+    --background: #f8f4e3;
+    --color: #272914;
+    --search-border: #d5573b;
+    --search-background: #648844;
+  }
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  background: var(--background);
+  color: var(--color);
+  min-width: 100vw;
+  min-height: 100vh;
 }
 </style>
---
+++ web/src/components/WikipediaTranslator.vue
@@ -0,0 +1,110 @@
+<template>
+  <header><h1>Wikipedia Translator</h1></header>
+  <main>
+    <form @submit="submit">
+      <label class="wide">
+        <span>Enter the term you want to translate:</span>
+        <input type="text" v-model="query" />
+      </label>
+      <label>
+        <span>From</span>
+        <select v-model="from">
+          <option value="en">English</option>
+          <option value="se">Swedish</option>
+        </select>
+      </label>
+      <label>
+        <span>To</span>
+        <select v-model="to">
+          <option value="se">Swedish</option>
+          <option value="en">English</option>
+        </select>
+      </label>
+      <output v-if="result">{{ result }}</output>
+    </form>
+  </main>
+  <footer>Made by @JRasmusBm</footer>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref } from "vue";
+// import { translate } from "../utils/translate";
+
+export default defineComponent({
+  setup() {
+    const query = ref("");
+    const from = ref("en");
+    const to = ref("se");
+    const result = ref<null | string>(null);
+
+    function submit(e) {
+      e.preventDefault();
+    }
+
+    return {
+      query,
+      from,
+      to,
+      submit,
+      result,
+    };
+  },
+});
+</script>
+
+<style scoped>
+header {
+  height: 4rem;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+main {
+  height: calc(100vh - 6rem);
+  display: grid;
+  grid-template-columns: auto 30rem auto;
+  grid-template-rows: 2fr auto 3fr;
+}
+
+main form {
+  grid-column: 2;
+  grid-row: 2;
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  grid-gap: 1rem;
+}
+
+main form span {
+  display: block;
+  padding: 1rem;
+}
+
+main form label.wide {
+  grid-column: 1 /-1;
+}
+
+main form input,
+main form select {
+  height: 4rem;
+  width: 100%;
+  border-radius: 1rem;
+  font-size: 2rem;
+  text-align: center;
+  text-align-last: center;
+  -moz-text-align-last: center;
+  border: 2px solid var(--color);
+  background: var(--search-background);
+  color: var(--color);
+}
+
+footer {
+  height: 2rem;
+  width: 100%;
+  position: fixed;
+  bottom: 0;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+</style>
